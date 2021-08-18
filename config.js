d08dc623f39f2fc86c3b7930bb2b83f90b6604b5
paulchainkang <paul@paul-kang.com>
2021-08-18 18:42:56 +0800

    wip

--- config.js
+++
@@ -1,9 +0,0 @@
-var local_api = true;
-
-if (local_api) {
-  var apiUrlBase = "http://localhost:5000/";
-} else {
-  var apiUrlBase = "https://api-for-sql.herokuapp.com/";
-}
-
-console.log(apiUrlBase);   
